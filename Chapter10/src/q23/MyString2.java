5c1744fd512ea326f877013b7e2fb889a6901058
sundisttt@gmail.com <sundisttt@gmail.com>
2022-07-25 22:12:31 +0300

    Strings

--- Chapter10/src/q23/MyString2.java
+++ Chapter10/src/q23/MyString2.java
@@ -1,4 +1,49 @@
 package q23;
 
 public class MyString2 {
+    private String s;
+
+    public MyString2(String s) {
+        this.s = "";
+        for (int i = 0; i < s.length(); i++) {
+            this.s += s.charAt(i) + "";
+        }
+    }
+
+    public int compare(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (this.s.charAt(i) < s.charAt(i)) return -1;
+            else if (this.s.charAt(i) > s.charAt(i)) return 1;
+        }
+        return 0;
+    }
+
+    public MyString2 substring(int begin) {
+        String sub = "";
+        for (int i = begin; i < s.length(); i++)
+            sub += s.charAt(i);
+
+        return new MyString2(sub);
+    }
+
+    public MyString2 toUpperCase() {
+        String str = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
+                str += String.valueOf((char) (s.charAt(i) - 32));
+            } else str += String.valueOf(s.charAt(i));
+        }
+        return new MyString2(str);
+    }
+
+    public char[] toChars() {
+        char[] chars = new char[s.length()];
+        for (int i = 0; i < s.length(); i++)
+            chars[i] = s.charAt(i);
+        return chars;
+    }
+
+    public static MyString2 valueOf(boolean b) {
+        return (b ? new MyString2("true") : new MyString2("false"));
+    }
 }
--- Chapter10/src/q23/Test.java
+++ Chapter10/src/q23/Test.java
@@ -2,6 +2,7 @@ package q23;
 
 public class Test {
     public static void main(String[] args) {
-
+        MyString2 ystr = new MyString2("Selam nasilsin");
+        System.out.println(ystr.toChars());
     }
 }
