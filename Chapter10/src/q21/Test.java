a172119c3ee0eb95316e57f662f8707ff4407ffa
Sundist <sundisttt@gmail.com>
2022-07-25 13:42:12 +0300

    Li Yang Book Chapters Initial

---
+++ Chapter10/src/q21/Test.java
@@ -0,0 +1,22 @@
+package q21;
+
+import java.math.BigInteger;
+
+public class Test {
+    public static void main(String[] args) {
+        int count = 0;
+        BigInteger n = new BigInteger(String.valueOf(Long.MAX_VALUE));
+        BigInteger five = new BigInteger("5");
+        BigInteger six = new BigInteger("6");
+        BigInteger zero = new BigInteger("0");
+        System.out.println("First ten numbers greater than Long.MAX_VALUE");
+        while (count < 10) {
+            n = n.add(new BigInteger("1"));
+            if ((n.remainder(five)).compareTo(zero) == 0 ||
+                    (n.remainder(six)).compareTo(zero) == 0) {
+                System.out.println(n);
+                count++;
+            }
+        }
+    }
+}
