cba860cce5a5b68c5a1dd8525c3126108096a6fb
Sundist <sundisttt@gmail.com>
2022-07-25 13:16:25 +0300

    Li Yang Book Chapters Initial

--- Chapter10/src/q18/Test.java
+++ Chapter10/src/q18/Test.java
@@ -1,4 +1,28 @@
 package q18;
 
+import java.math.BigInteger;
+
 public class Test {
+    public static void main(String[] args) {
+        BigInteger number = BigInteger.valueOf((long) Long.MAX_VALUE);
+        int counter = 0;
+        while (counter < 5) {
+            number = number.add(BigInteger.valueOf(1));
+            if (isPrime(number)) {
+                System.out.println(number);
+                counter++;
+            }
+        }
+    }
+
+    public static boolean isPrime(BigInteger n) {
+        for (BigInteger d = new BigInteger("2");
+             d.compareTo(n.divide(new BigInteger("2"))) <= 0;
+             d = d.add(new BigInteger("1"))) {
+            if (n.remainder(d).compareTo(new BigInteger("0")) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
