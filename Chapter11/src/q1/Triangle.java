e5cf89b6df8812577127713815ed05126fabbfe9
Sundist <sundisttt@gmail.com>
2022-07-31 15:11:58 +0300

    Chapter 11

--- Chapter11/src/q1/Triangle.java
+++ Chapter11/src/q1/Triangle.java
@@ -1,4 +1,61 @@
 package q1;
 
-public class Triangle extends GeometricObject{
+public class Triangle extends GeometricObject {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    Triangle() {
+        side1 = 1;
+        side2 = 1;
+        side3 = 1;
+    }
+
+    Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double getSide1() {
+        return this.side1;
+    }
+
+    public double getSide2() {
+        return this.side2;
+    }
+
+    public double getSide3() {
+        return this.side3;
+    }
+
+    public void setSide1(double side1) {
+        this.side1 = side1;
+    }
+
+    public void setSide2(double side2) {
+        this.side2 = side2;
+    }
+
+    public void setSide3(double side3) {
+        this.side3 = side3;
+    }
+
+    public double getArea() {
+        double u = (getSide1() + getSide2() + getSide3()) / 2;
+        return Math.sqrt(u * (u - getSide1()) * (u - getSide2()) * (u - getSide3()));
+    }
+
+    public double getPerimeter() {
+        return (getSide1() + getSide2() + getSide3()) / 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "side1=" + side1 +
+                ", side2=" + side2 +
+                ", side3=" + side3 +
+                '}';
+    }
 }
