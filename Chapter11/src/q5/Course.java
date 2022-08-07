a3c4b7d7641ecc37ebf9113c656832c28ddf1171
Sundist <sundisttt@gmail.com>
2022-08-07 14:55:26 +0300

    Chapter 11

--- Chapter11/src/q5/Course.java
+++ Chapter11/src/q5/Course.java
@@ -1,5 +1,34 @@
 package q5;
 
+import java.util.ArrayList;
+
 public class Course {
-    
+    private String course;
+    private ArrayList<String> students;
+
+    Course(String course) {
+        this.course = course;
+        students = new ArrayList<String>();
+    }
+
+    public String getCourseName() {
+        return course;
+    }
+
+    public void addStudent(String student) {
+        students.add(student);
+    }
+
+    public void dropStudent(String student) {
+        students.remove(student);
+    }
+
+    public String[] getStudent() {
+        String[] s = new String[students.size()];
+        return students.toArray(s);
+    }
+
+    public int getNumberOfStudents() {
+        return students.size();
+    }
 }
