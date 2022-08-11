7ec75688302797e9bad677758a467f17f606803b
Sundist <sundisttt@gmail.com>
2022-08-11 13:33:33 +0300

    Chapter 11

---
+++ Chapter11/src/q13/Main.java
@@ -0,0 +1,28 @@
+package q13;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter 10 numbers to add array");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < 10; i++)
+            arr.add(scanner.nextInt());
+        System.out.println(arr + "\nAfter duplicates removed");
+        removeDuplicate(arr);
+
+    }
+
+    // 4, 5, 8, 1, 3, 4, 4, 5, 8, 9
+    public static void removeDuplicate(ArrayList<Integer> list) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (Integer element : list) {
+            if (!arr.contains(element))
+                arr.add(element);
+        }
+        System.out.println(arr);
+    }
+
+}
