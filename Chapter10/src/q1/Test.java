5bb5730edfb72d1994f66300cecfd849a1a9bbf4
Sundist <sundisttt@gmail.com>
2022-09-06 11:03:11 +0300

    Chapter 11 completed

--- Chapter10/src/q1/Test.java
+++ Chapter10/src/q1/Test.java
@@ -2,9 +2,9 @@ package q1;
 
 public class Test {
     public static void main(String[] args) {
-        Time time1 = new Time();
+        Time t1 = new Time();
         Time time2 = new Time(555550000);
-        System.out.println(time1.getHour() + ":" + time1.getMinute() + ":" + time1.getSecond());
+        System.out.println(t1.getHour() + ":" + t1.getMinute() + ":" + t1.getSecond());
         System.out.println(time2.getHour() + ":" + time2.getMinute() + ":" + time2.getSecond());
     }
 }
