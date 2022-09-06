fecdb7a7cd853ca466ee1cf2a0fcfcf8edee9e42
Sundist <sundisttt@gmail.com>
2022-09-06 11:04:18 +0300

    Chapter 11 completed

--- Chapter11/src/q1/Test.java
+++ Chapter11/src/q1/Test.java
@@ -2,7 +2,7 @@ package q1;
 
 public class Test {
     public static void main(String[] args) {
-        Triangle t1 = new Triangle();
-        System.out.println(t1.toString());
+        Triangle triangle1 = new Triangle();
+        System.out.println(triangle1.toString());
     }
 }
