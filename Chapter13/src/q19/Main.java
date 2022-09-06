7f22841f65b84487b6c906c623208c996a7b7828
Sundist <sundisttt@gmail.com>
2022-09-06 10:57:05 +0300

    Chapter 13.19 completed

--- Chapter13/src/q19/Main.java
+++ Chapter13/src/q19/Main.java
@@ -1,7 +1,22 @@
 package q19;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a decimal number: ");
+        String[] decimal = input.nextLine().split("[.]");
+
+        Rational r1 = new Rational(new BigInteger(decimal[0]), BigInteger.ONE);
+
+        Rational r2 = new Rational(new BigInteger(decimal[1]), new BigInteger(
+                String.valueOf((int) Math.pow(10, decimal[1].length()))));
+
+        System.out.println("The fraction number is " +
+                (decimal[0].charAt(0) == '-' ? (r1).subtract(r2) : (r1).add(r2)));
 
     }
 }
